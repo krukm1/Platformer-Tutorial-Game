0019b9b2ecddca999ae83833eb4abe80d0934bfd
Mateusz <matt130pl@gmail.com>
2025-07-16 21:57:55 -0500

    almost done

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
 
-func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin!")
+func _on_body_entered(_body):
+	game_manager.add_point()
 	queue_free()
